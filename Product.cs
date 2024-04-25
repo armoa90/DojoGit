1f42cde11790dbb2b9d3212709d50240cde4ac6b
Armoa <armoa1590@gmail.com>
2024-04-25 11:11:59 -0300

    commit pruebas

--- Product.cs
+++ Product.cs
@@ -1,6 +1,6 @@
 public class Product
-{
-    public int id { get; set; }
+{   
+     public int id { get; set; }
     public int descripcion { get; set; }
     public decimal precio { get; set; }
 }
