55fc8437af981c385ac641810d6bc32db009dd84
Armoa <armoa1590@gmail.com>
2024-04-25 11:19:56 -0300

    complete clases

---
+++ Employee.cs
@@ -0,0 +1,7 @@
+public class Employee
+{
+    public int id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public decimal Salario { get; set; }
+}
